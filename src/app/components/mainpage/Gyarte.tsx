4a070a01cf5f93b1bc9434974862e8be1439ca2a
zushii <zxgamingh@gmail.com>
2025-01-13 10:19:57 +0100

    minor adjustment to sizing for desktop

--- src/app/components/mainpage/Gyarte.tsx
+++ src/app/components/mainpage/Gyarte.tsx
@@ -3,11 +3,13 @@ import Button from "../general/Button";
 export default function GyarteSection(){
   return (
 
-    <section className="bg-dark text-light p-5 xl:px-80">
+    <section className="bg-dark text-light p-5 xl:px-60 xl:flex min-h-60">
+    <div className="px-20">
+      <h2 className="font-bold text-3xl w-full text-center mb-4">GymnasieArbete</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur alias ipsum, nostrum quas cupiditate provident tempore culpa tenetur sapiente consequuntur maxime ducimus soluta eligendi nobis perferendis placeat fugit. Quis, assumenda.</p>
+    </div>
 
-    <h2 className="font-bold text-3xl w-full text-center mb-4">GymnasieArbete</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur alias ipsum, nostrum quas cupiditate provident tempore culpa tenetur sapiente consequuntur maxime ducimus soluta eligendi nobis perferendis placeat fugit. Quis, assumenda.</p>
-    <div className="flex flex-col justify-center items-center gap-5 mt-7 mb-8">
+    <div className="flex flex-col justify-center items-center gap-5 mt-7 mb-8 m-20 xl:gap-9">
       <Button link="/teknikprogram" text="Teknikprogram" color="bg-complementary" hColor="hover:bg-btnHoverYl"/>
       <Button link="/estetprogram" text="Estetprogram" color="bg-complementary" hColor="hover:bg-btnHoverYl"/>
     </div>
--- src/app/components/mainpage/StartSection.tsx
+++ src/app/components/mainpage/StartSection.tsx
@@ -3,11 +3,11 @@ import Button from "../general/Button";
 export default function StartSection(){
   return (
 
-    <section className="h-screen top-0 bg-center bg-cover flex justify-center items-center text-light text-center flex-col" style={{backgroundImage:`url("/lbs-kreativa-gymnasiet-stockholm-sodra-1920x1080.jpg")`}}>
+    <section className="h-screen top-0 bg-center bg-cover flex justify-center items-center text-light text-center flex-col7 lg:text-left lg:justify-start" style={{backgroundImage:`url("/lbs-kreativa-gymnasiet-stockholm-sodra-1920x1080.jpg")`}}>
 
-      <div className="bg-black bg-opacity-50 p-2">
-        <h1 className="text-4xl font-bold">LBS Kreativa<br/>Gymnasiet <br/>Stockholm Södra</h1>
-        <h2 className="text-2xl">Gymnasie Arbete <br className="xl:hidden"/>&<br className="xl:hidden"/>Programveckor</h2>        
+      <div className="bg-black bg-opacity-50 p-4 lg:ml-20">
+        <h1 className="text-4xl font-bold md:text-6xl">LBS Kreativa<br/>Gymnasiet <br/>Stockholm Södra</h1>
+        <h2 className="text-2xl md:text-4xl lg:mt-4">Gymnasie Arbete <br className="lg:hidden"/>& <br className="lg:hidden"/>Programveckor</h2>        
       </div>
 
 
