995cc8e33c51ec336ed9eb1af1cc9372047d3cfd
zushii <zxgamingh@gmail.com>
2025-01-13 10:42:12 +0100

    mainpage button fix and slight padding adjustment

--- src/app/components/mainpage/Gyarte.tsx
+++ src/app/components/mainpage/Gyarte.tsx
@@ -3,15 +3,15 @@ import Button from "../general/Button";
 export default function GyarteSection(){
   return (
 
-    <section className="bg-dark text-light p-5 xl:px-60 xl:flex min-h-60">
-    <div className="px-20">
+    <section className="bg-dark text-light p-16 xl:px-60 xl:flex min-h-60">
+    <div>
       <h2 className="font-bold text-3xl w-full text-center mb-4">GymnasieArbete</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur alias ipsum, nostrum quas cupiditate provident tempore culpa tenetur sapiente consequuntur maxime ducimus soluta eligendi nobis perferendis placeat fugit. Quis, assumenda.</p>
     </div>
 
     <div className="flex flex-col justify-center items-center gap-5 mt-7 mb-8 m-20 xl:gap-9">
-      <Button link="/teknikprogram" text="Teknikprogram" color="bg-complementary" hColor="hover:bg-btnHoverYl"/>
-      <Button link="/estetprogram" text="Estetprogram" color="bg-complementary" hColor="hover:bg-btnHoverYl"/>
+      <Button link="/teknikprogram" text="Teknikprogram" customClasses="bg-complementary hover:bg-btnHoverYl w-40 text-dark font-bold"/>
+      <Button link="/estetprogram" text="Estetprogram" customClasses="bg-complementary hover:bg-btnHoverYl w-40 text-dark font-bold"/>
     </div>
 
     </section>
--- src/app/components/mainpage/PVSection.tsx
+++ src/app/components/mainpage/PVSection.tsx
@@ -4,14 +4,14 @@ export default function ProgramveckorSection(){
 
   return (
 
-    <section className="p-5 flex xl:px-60">
+    <section className="p-16 flex xl:px-60">
 
     
       <div className="flex flex-col items-center justify-center mb-8 md:px-5">
         <h2 className="font-bold text-3xl w-full text-center mb-4">Programveckor</h2>
         <p className="mb-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus illo libero fugit obcaecati doloremque, modi, ex perferendis neque est nisi labore iusto temporibus eum aspernatur cumque optio ullam velit porro?</p>        
 
-        <Button link="/programveckor" text="Mer" color="bg-dark" hColor="hover:bg-btnHoverDark"/>
+        <Button link="/programveckor" text="Mer" customClasses="bg-dark hover:bg-btnHoverDark w-40 font-bold text-light"/>
       </div>
 
       <picture className="md:block hidden"> {/*add lower resolution*/}
