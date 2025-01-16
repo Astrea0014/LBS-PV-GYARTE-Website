41337c4d20ffb93723246ac10dd3da085a81bd77
Wilmer Säfsten <Wilmer.Safsten@gmail.com>
2025-01-16 08:29:21 +0100

    Single programweek project page

--- src/app/programveckor/[collabId]/[projectId]/SuSg.tsx
+++ src/app/programveckor/[collabId]/[projectId]/SuSg.tsx
@@ -1,16 +1,19 @@
 import PageHeader from "@/app/components/general/PageHeader";
 import PageDescription from "@/app/components/general/PageDescription";
 import Whitespace from "@/app/components/general/navbar/NavWhitespace";
-import Button from "@/app/components/general/Button";
+import { Video } from "@/app/components/progrmveckor/Video";
+import AltButton from "@/app/components/progrmveckor/AltButton";
+
 
 interface SuSgProps {
   gameTitle: string;
   overview: string;
   posterRef?: string;
   itchLink: string;
+  videoRef?: string;
 }
 
-export default function SuSg({gameTitle, overview, posterRef, itchLink}: SuSgProps) {
+export default function SuSg({gameTitle, overview, posterRef, itchLink, videoRef}: SuSgProps) {
   return (
     <main>
       <Whitespace />
@@ -23,9 +26,18 @@ export default function SuSg({gameTitle, overview, posterRef, itchLink}: SuSgPro
         <div className="w-5/6 sm:w-4/6 mt-8">
           <PageHeader headerTitle={gameTitle} centerd />
           <PageDescription description={overview} />
-          <Button customClasses={"bg-complementary text-black mx-auto my-8"} link={itchLink} text="Play game" />
+          <AltButton customClasses={"bg-complementary text-black mx-auto my-8 py-2 px-4"} link={itchLink} text="Play&nbsp;game" />
         </div>
       </section>
+      
+      <section className="my-24 flex flex-col items-center justify-center bg-dark py-24">
+        <PageHeader colorClass="text-white" headerTitle="Video av arbetet" centerd /> 
+        <div className="text-white">
+          <Video videoRef={videoRef}/>
+        </div>
+      </section>
+
+      <AltButton link={itchLink} text="Play&nbsp;game" customClasses={"bg-complementary text-black mx-auto my-8 px-32 py-4 text-xl"} />
     </main>
   );
 }
