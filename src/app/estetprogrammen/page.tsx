8df99b0df35559b41afbc322f56f4c559a77b815
zushii <zxgamingh@gmail.com>
2025-01-17 10:54:07 +0100

    renamed folders

--- src/app/estetprogrammen/page.tsx
+++ src/app/estetprogrammen/page.tsx
@@ -5,7 +5,7 @@ export default function Estetprogram(){
   return(
 
     <main>
-      <Header bgImage="/lbs-kreativa-gymnasiet-stockholm-sodra-1920x1080.jpg" program="Estetprogramen"/>
+      <Header bgImage="/lbs-kreativa-gymnasiet-stockholm-sodra-1920x1080.jpg" program="Estetprogrammen"/>
       <ul className="flex flex-wrap justify-center p-5 gap-20 my-16">
         <Card focus="GRAFISK DESIGN" image="/gyarte/lbs-grafisk-design-400x300.jpg" link="/gymnasiearbeten?inriktning=GD" />
         <Card focus="SPELGRAFIK" image="/gyarte/lbs-spelgrafik-400x300.jpg" link="/gymnasiearbeten?inriktning=SG" />
