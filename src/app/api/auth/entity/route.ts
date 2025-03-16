919113e34eb92abdd91e9500f25e571d39aa2fd1
Douglas Lyman <douglas.lyman@icloud.com>
2025-03-16 19:14:26 +0100

    Add route structure

---
+++ src/app/api/auth/entity/route.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function POST(request: NextRequest) {
+
+}
+
+export function PATCH(request: NextRequest) {
+
+}
+
+export function GET(request: NextRequest) {
+  
+}
