154fe2e0877d385e1545d0136eaae3e1b1fc5664
Douglas Lyman <douglas.lyman@icloud.com>
2025-03-17 01:05:54 +0100

    Started merging and refining database code and finished PV

---
+++ src/app/lib/database/Database.ts
@@ -0,0 +1,188 @@
+import mysql from "mysql2/promise";
+import { errors } from "./Errors";
+import { Collaboration, FullCollaboration, GroupMember, ProjectGroup } from "./DbTypes";
+
+export class Database {
+  private connection: mysql.Connection | undefined;
+  
+  private pvDataRequesters: Map<string, (id: number, connection: mysql.Connection) => Promise<any>>;
+  private gyDataRequesters: Map<string, (id: number, connection: mysql.Connection) => Promise<any>>;
+  
+  private async GetPresentYears(query: string) {
+    if (!this.connection)
+      throw new Error(errors.not_connected);
+
+    return this.connection.execute<mysql.RowDataPacket[]>(query)
+    .then((result) => {
+      return result[0].map((value) => value.year as number);
+    });
+  }
+
+  private async PVGetCollaborationsFromQueryString(query: string, data: number): Promise<Collaboration[]> {
+    if (!this.connection)
+      throw new Error(errors.not_connected);
+  
+    const collaborations = await this.connection.execute<mysql.RowDataPacket[]>(
+      "SELECT * FROM collaborations WHERE " + query + "=?",
+      data
+      );
+  
+    const ret: Collaboration[] = [];
+  
+    for (let i = 0; i < collaborations[0].length; i++) {
+      const collaborator = await this.connection.query<mysql.RowDataPacket[]>(
+        "SELECT collaborator FROM collaborators WHERE collaboration_id=?",
+        collaborations[0][i].collaboration_id
+        );
+  
+        ret.push({
+          collaboration_id: collaborations[0][i].collaboration_id,
+          year: collaborations[0][i].year,
+          theme: collaborations[0][i].theme,
+          description: collaborations[0][i].description,
+          poster_ref: collaborations[0][i].poster_ref,
+          collaborators: collaborator[0].map((value) => value.collaborator as string)
+        });
+    }
+  
+    return ret;
+  }
+
+  private async PVGetGroupMembersFromProjectId(project_id: number): Promise<GroupMember[]> {
+    return this.connection!.execute<mysql.RowDataPacket[]>(
+      "SELECT name, class FROM project_groups_people INNER JOIN people ON project_groups_people.person_id=people.person_id WHERE project_id=?",
+      project_id
+    ).then(result => {
+      return result[0].map((value): GroupMember => {
+        return {
+          name: value.name,
+          class: value.class
+        };
+      });
+    });
+  }
+
+  public constructor() {
+    this.connection = undefined;
+    this.pvDataRequesters = new Map();
+    this.gyDataRequesters = new Map();
+  }
+
+  public async Connect(): Promise<void> {
+    if (this.connection)
+      throw new Error(errors.already_connected);
+
+    this.connection = await mysql.createConnection({
+      host: process.env.MYSQL_HOST,
+      user: process.env.MYSQL_USER,
+      database: process.env.MYSQL_DB_NAME,
+      password: process.env.MYSQL_PASSWORD
+    });
+
+    return this.connection.connect();
+  }
+
+  public Disconnect() {
+    if (!this.connection)
+      throw new Error(errors.not_connected);
+
+    this.connection.destroy();
+    this.connection = undefined;
+  }
+
+  public IsConnected(): boolean {
+    return this.connection != undefined;
+  }
+
+  public GetConnection(): mysql.Connection {
+    if (!this.connection)
+      throw new Error(errors.not_connected);
+    return this.connection;
+  }
+
+  public SetPVDataRequester(layout_reference: string, callback: (id: number, connection: mysql.Connection) => Promise<any>) {
+    if (!this.pvDataRequesters.has(layout_reference))
+      this.pvDataRequesters.set(layout_reference, callback);
+  }
+
+  public SetGYDataRequester(layout_reference: string, callback: (id: number, connection: mysql.Connection) => Promise<any>) {
+    if (!this.gyDataRequesters.has(layout_reference))
+      this.gyDataRequesters.set(layout_reference, callback);
+  }
+
+  public async PVGetPresentYears(): Promise<number[]> {
+    return this.GetPresentYears("SELECT year FROM collaborations GROUP BY year")
+  }
+
+  public async PVGetCollaborationsFromYear(year: number): Promise<Collaboration[]> {
+    return this.PVGetCollaborationsFromQueryString(
+      "year",
+      year
+    );
+  }
+
+  public async PVGetCollaborationFromId(collaboration_id: number): Promise<FullCollaboration> {
+    const collaborations = await this.PVGetCollaborationsFromQueryString(
+      "collaboration_id",
+      collaboration_id
+    );
+
+    if (collaborations.length == 0)
+      throw new Error(errors.result_empty);
+
+    return {
+      ...collaborations[0],
+      project_groups: await this.connection!.execute<mysql.RowDataPacket[]>(
+        "SELECT * FROM project_groups WHERE collaboration_id=?",
+        collaboration_id
+      ).then(async (result): Promise<ProjectGroup[]> => {
+        const ret: ProjectGroup[] = [];
+        
+        for (let i = 0; i < result[0].length; i++) {
+          ret.push({
+            project_id: result[0][i].project_id,
+            project_name: result[0][i].project_name,
+            group_name: result[0][i].group_name,
+            poster_ref: result[0][i].poster_ref,
+            description: result[0][i].description,
+            project_type: result[0][i].project_type,
+            project_data: null,
+            group_members: await this.PVGetGroupMembersFromProjectId(result[0][i].project_id)
+          });
+        }
+
+        return ret;
+      })
+    };
+  }
+
+  public async PVGetProjectFromId(project_id: number): Promise<ProjectGroup> {
+    if (!this.connection)
+      throw new Error(errors.not_connected);
+
+    const projects = await this.connection.execute<mysql.RowDataPacket[]>(
+      "SELECT * FROM project_groups WHERE project_id=?",
+      project_id
+    );
+
+    if (projects[0].length == 0)
+      throw new Error(errors.result_empty);
+
+    const ret: ProjectGroup = {
+      project_id: projects[0][0].project_id,
+      project_name: projects[0][0].project_name,
+      group_name: projects[0][0].group_name,
+      poster_ref: projects[0][0].poster_ref,
+      description: projects[0][0].description,
+      project_type: projects[0][0].project_type,
+      project_data: null,
+      group_members: await this.PVGetGroupMembersFromProjectId(projects[0][0].project_id)
+    };
+
+    if (!this.pvDataRequesters.has(ret.project_type))
+      throw new Error(errors.data_requester_not_exists(ret.project_type));
+    ret.project_data = (this.pvDataRequesters.get(ret.project_type)!)(ret.project_id, this.connection);
+
+    return ret;
+  }
+}
