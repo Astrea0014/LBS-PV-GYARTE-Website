dce948fd658881e2cbf0136503054a9d8b83d28d
Douglas Lyman <douglas.lyman@icloud.com>
2025-04-24 23:41:48 +0200

    Made sure that every endpoint without an implementation returns 502 when called

--- src/app/api/auth/upload/route.ts
+++ src/app/api/auth/upload/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
+import { HTTP_CODES } from "@/app/lib/Errors";
 
 export function POST(request: NextRequest) {
-  
+  return HTTP_CODES.not_implemented();
 }
