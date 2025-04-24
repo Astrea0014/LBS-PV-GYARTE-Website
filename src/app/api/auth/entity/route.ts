958a97abf57df2f21ee422c3fda15299519df8ce
Douglas Lyman <douglas.lyman@icloud.com>
2025-04-24 23:40:32 +0200

    Added route that allows for changing account information

--- src/app/api/auth/entity/route.ts
+++ src/app/api/auth/entity/route.ts
@@ -1,13 +1,69 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function POST(request: NextRequest) {
+import { AuthException, ContentException, HTTP_CODES, ResultException, SqlException } from "@/app/lib/Errors";
+import { GetTokenFromRequestCookie } from "@/app/lib/RoutingHelpers";
 
+import { GetEntityACL } from "@/app/lib/authentication/AccessControl";
+import { AuthenticatedUser, UpdateEntityActionArgs } from "@/app/lib/authentication/AuthenticatedUser";
+
+export function POST(request: NextRequest) {
+  // Register entity.
+
+  return HTTP_CODES.not_implemented();
 }
 
-export function PATCH(request: NextRequest) {
+export async function PATCH(request: NextRequest) {
+  // Update password and access rights for entity.
 
+  try {
+    // 1. Authenticate
+
+    const token = await GetTokenFromRequestCookie(request);
+    const acl = await GetEntityACL(token.entity_id);
+
+    const user = new AuthenticatedUser(token, acl);
+
+    // 2. Validate body
+
+    const body = await request.json();
+
+    if (!("username" in body))
+      throw new SyntaxError("Required field 'username' is missing from body.");
+    if (!("modify" in body))
+      throw new SyntaxError("Required field 'modify' is missing from body.");
+
+    // 3. Update entity.
+
+    const result = await user.UpdateEntity(body as UpdateEntityActionArgs);
+    return NextResponse.json(result);
+  }
+  catch (e) {
+    // If the token is missing, expired or invalid.
+    if (e instanceof AuthException)
+      return HTTP_CODES.unauthorized(e.message);
+
+    // If the JSON-body is invalid or has an invalid structure.
+    if (e instanceof SyntaxError || e instanceof ContentException)
+      return HTTP_CODES.bad_request(e.message);
+
+    // If the entity does not exist.
+    if (e instanceof ResultException)
+      return HTTP_CODES.not_found(e.message);
+
+    // If the SQL-executes are throwing.
+    if (e instanceof SqlException) {
+      console.error(e.message);
+      return HTTP_CODES.bad_gateway();
+    }
+
+    return HTTP_CODES.internal_server_error();
+  }
 }
 
 export function GET(request: NextRequest) {
+  // Get usernames of registered entities.
+  // OR
+  // Get username and access for single entity.
   
+  return HTTP_CODES.not_implemented();
 }
